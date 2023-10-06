52e1b06bb284b1acd6cd3d4172043dc597eb0007
dallinfunn <funndallin@gmail.com>
2023-10-06 01:25:51 -0500

    updated urls to include API view

--- Capstone-project/restaurant/views.py
+++ Capstone-project/restaurant/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from.serializers import BookingSerializer, MenuSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Booking, Menu
 
 # Create your views here.
+
 def index (request):
     return render(request, 'index.html', {})
 
+class BookingView(APIView):
+
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+    
+class MenuView(APIView):
+    
+    def post(self, request):
+        serializer = MenuSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
